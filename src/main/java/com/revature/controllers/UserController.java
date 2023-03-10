a0ff3e96ccaaf9cf2ae80b927aafbbbf15765d50
Joey Wham <joewham14@gmail.com>
2023-03-10 11:54:58 -0500

    added another method

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -25,7 +25,7 @@ import com.revature.services.UserService;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:3000"}, allowCredentials = "true")
 public class UserController {
 
 	private final AuthService authService;
